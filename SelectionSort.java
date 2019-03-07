f3d529e0339fa7fe97dce861bd758ac5cbb13d3c
yerinthevarajah <47553515+yerinthevarajah@users.noreply.github.com>
2019-03-07 13:57:45 -0500

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,67 @@
+package cas.lab8.wt;
+
+import java.util.Comparator;
+
+public class SelectionSort {
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (less(a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+            assert isSorted(a);
+        }
+        assert isSorted(a);
+    }
+
+
+
+   /***************************************************************************
+    *  Helper sorting functions.
+    ***************************************************************************/
+    
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+        
+        
+    // exchange a[i] and a[j]
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+   /***************************************************************************
+    *  Check if array is sorted - useful for debugging.
+    ***************************************************************************/
+
+        
+    // is the array sorted from a[lo] to a[hi]
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+
+
+
+    // print array to standard output
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+
+}
